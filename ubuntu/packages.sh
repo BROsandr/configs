2f054221858eacc971262dac5a5914f12b49c310
alexkharl <sasha_xarlamov@mail.ru>
2024-04-18 13:32:05 +0300

    Feat(ubuntu):Доб-ие packages.sh

---
+++ ubuntu/packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install build-essentials
+sudo apt install gcc
+sudo apt install clang
+sudo apt install stow
+sudo apt install verilator
+sudo apt install pip
+sudo apt install texlive-full
