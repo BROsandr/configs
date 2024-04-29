999e5f10f53d940e2c635e202940182bf4bfc514
alexkharl <sasha_xarlamov@mail.ru>
2024-04-29 21:58:43 +0300

    Feat(nix/hm/fish):Доб-ие плагина timer

--- nix/home-manager/apps/fish.nix
+++ nix/home-manager/apps/fish.nix
@@ -1,5 +1,22 @@
+{ pkgs, ... }:
 {
   programs.fish = {
     enable = true;
+    plugins = [
+      # oh-my-fish plugins are stored in their own repositories, which
+      # makes them simple to import into home-manager.
+      {
+        name = "timer";
+        src = pkgs.fetchFromGitHub {
+          owner = "jichu4n";
+          repo = "fish-command-timer";
+          rev = "ba68bd0a1d06ea99aadefe5a4f32ff512783d432";
+          hash = "sha256-Ip677gZlcO8L/xukD7Qoa+C+EcI2kGd+BSOi2CDOzM4=";
+        };
+      }
+    ];
+    shellInitLast = ''
+      set fish_command_timer_time_format '%b %d %H:%M'
+    '';
   };
 }
