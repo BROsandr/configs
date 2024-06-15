7bd3a015a8fd2bffa629d9143e189d6973b0b86d
alexkharl <sasha_xarlamov@mail.ru>
2024-06-15 22:02:58 +0300

    Ref(nix/hm/fonts):Уст-ка defaultFonts

--- nix/home-manager/main/apps/fonts.nix
+++ nix/home-manager/main/apps/fonts.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }: {
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts.monospace = [ "JetBrainsMonoNL Nerd Font" ];
+  };
   home.packages  = with pkgs; [
     nerdfonts
   ];
