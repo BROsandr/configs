534121e77f9f6b4f7638daebf796aaf6cc44b693
alexkharl <sasha_xarlamov@mail.ru>
2024-06-15 20:13:51 +0300

    Fix(nix/hm/bash):Откл-ие fish в логине

--- nix/home-manager/main/apps/bash.nix
+++ nix/home-manager/main/apps/bash.nix
@@ -1,7 +1,9 @@
 {
   programs.bash = {
     enable = true;
-    profileExtra = "fish";
+
+    # We cannot support the overpowered fish with ttf in tty so fallback to bash.
+    profileExtra = if builtins.pathExists /etc/NIXOS then "" else "fish";
   };
 
   programs.direnv.enableBashIntegration = true;
