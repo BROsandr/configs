7b7338a54aa0b2b4308f98fd82454416af3374d1
BROsandr <53278658+BROsandr@users.noreply.github.com>
2024-09-04 23:10:14 +0300

    Fix(nix/conf/users):Обн-ие hashed_passwd

--- nix/configuration/users.nix
+++ nix/configuration/users.nix
@@ -5,6 +5,6 @@
     isNormalUser = true;
     home = "/home/alex";
     extraGroups = [ "wheel" ];
-    hashedPassword = "$y$j9T$ls1MRosiRSeCCoNpjrj0b0$IcRRJZz22u9tNtzVqNY6FSJ8JLUp/ERqEsj2fLy5nhD";
+    hashedPassword = "$y$j9T$iTvm77DrAaiZ2azO.dIk5/$DRksoKrOCLC7ZVc/HhEMxY8cV6IEtgkydnsq6qdINgC";
   };
 }
