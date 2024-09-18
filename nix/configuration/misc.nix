3769eda2026b2d979b7df6e51068b20857f76542
alexkharl <sasha_xarlamov@mail.ru>
2024-09-18 22:26:06 +0300

    Feat(nix/conf/misc):Доб-ие polkit и keyring

--- nix/configuration/misc.nix
+++ nix/configuration/misc.nix
@@ -1,6 +1,19 @@
-{
+{ pkgs, ... } : {
   networking.hostName = "nixalex";
   time.timeZone = "Europe/Moscow";
   nixpkgs.config.allowUnfree = true;
-  services.gnome-keyring.enable = true;
+
+  environment.systemPackages = with pkgs; [ polkit lxqt.lxqt-policykit libsForQt5.kwallet libsForQt5.kwalletmanager ];
+
+  security = {
+    pam.services.kwallet = {
+      name = "kwallet";
+      enableKwallet = true;
+    };
+  };
+
+  security.pam.services.sddm.enableKwallet = true;
+  security.pam.services.login.enableKwallet = true;
+
+  security.polkit.enable = true;
 }
